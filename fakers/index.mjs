a040a4cdd86cb9a2a8e12469e43fb6f734017762
haritiana adriano <hei.adriano.4@gmail.com>
2022-08-01 14:33:37 +0300

    faker 1.1

---
+++ fakers/index.mjs
@@ -0,0 +1,36 @@
+import { faker } from "@faker-js/faker";
+import fs from "fs";
+/////////////////////////Here begin the code////////////////////////
+
+let insertion = "INSERT INTO account "+
+                "("+
+                    "first_name,last_name,nickname,birthday,gender,email,profile_pic"+
+                ")";
+
+let intercalaire= " VALUES ";
+
+let data= `\n('${faker.name.firstName()}','${faker.name.lastName()}','${faker.internet.userName()}','${faker.helpers.arrayElement(['M', 'F'])}','${faker.internet.email()}','${faker.image.avatar().replace(".jpg", ".png")}')`
+
+for(let i=1; i<=500_000; i++){
+    let components = {
+        "firstName": faker.name.firstName(),
+        "lastName": faker.name.lastName(),
+        "userName": faker.internet.userName(),
+        "gender": faker.helpers.arrayElement(['M','F']),
+        "email": faker.internet.email(),
+        "image": faker.image.avatar().replace(".jpg", ".png")
+    }
+
+    data += `,\n('${components.firstName}','${components.lastName}','${components.userName}', '${components.gender}','${components.email}','${components.image})`
+}
+data += ';';
+
+let generate = insertion + intercalaire + data;
+
+//////////////////////////////Go to generate the file of data.sql//////////////////////////////////////
+
+fs.writeFile('data.sql', generate, (err) => {
+    if(err){
+        console.log(err);
+    }
+})
